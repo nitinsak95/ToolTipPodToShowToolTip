ab1f9d268d3826a8e5f9abab70bd2499f0b46c5a
affixusimac1 <viveking@outlook.com>
2018-10-10 14:45:06 +0530

    123

--- ToolTip/ViewController.swift
+++ ToolTip/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import AMPopTip
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var customView: UIView!
+    @IBOutlet weak var collectioView: UICollectionView!
+    @IBOutlet var oView: UIView!
+    
+    let popTip = PopTip()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       collectioView.delegate = self
+        collectioView.dataSource = self
+        
+        collectioView.reloadData()
+        
+        popTip.bubbleColor = .green
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btShowPopUp(_ sender: UIButton) {
+        
+        popTip.show(customView: oView, direction: .down, in: view, from: sender.frame)
     }
-
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return  8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! tPCollectionViewCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! tPCollectionViewCell
+   
+////        cell.btTp.tag = indexPath.row
+//       cell.btTp.addTarget(self, action: #selector(self.btnClick(_:)), for: .touchUpInside)
+        
+        
+        popTip.show(customView: customView, direction: .down, in: view, from: cell.frame)
+    }
+    
+//    @objc func btnClick(_ sender:UIButton) {
+//        print("My custom button action")
+//         popTip.show(customView: customView, direction: .down, in: view, from: sender.frame)
+//    }
+    
 }
 
---
+++ tPCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  tPCollectionViewCell.swift
+//  ToolTip
+//
+//  Created by AFFIXUS IMAC1 on 6/4/18.
+//  Copyright © 2018 AFFIXUS IMAC1. All rights reserved.
+//
+
+import UIKit
+
+class tPCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var btTp: UIButton!
+    @IBOutlet weak var labell: UILabel!
+    @IBOutlet weak var labelagain: UILabel!
+    @IBOutlet weak var vw: UIView!
+ 
+    @IBAction func butonclick(_ sender: UIButton) {
+       
+    }
+}
